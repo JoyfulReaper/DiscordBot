bca7aa000017c7ab5603a652cea2ddd355bf2a3e
Kyle Givler <xvegan88x@gmail.com>
2021-04-15 22:17:59 -0400

    Skeleton

--- DiscordBot/DiscordBot/Program.cs
+++ DiscordBot/DiscordBot/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace DiscordBot
 {
     class Program
     {
-        static void Main(string[] args)
+        private static DiscordSocketClient _client;
+
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            //var token = "tokenHere";
+            var token = File.ReadAllText(@"C:\token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private static Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
