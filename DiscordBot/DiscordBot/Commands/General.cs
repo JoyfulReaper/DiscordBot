abc49081eb50dbfbdbef1c2179653138a70200f0
Kyle Givler <xvegan88x@gmail.com>
2021-04-24 22:50:26 -0400

    cleanup

--- DiscordBot/DiscordBot/Commands/General.cs
+++ DiscordBot/DiscordBot/Commands/General.cs
@@ -1,10 +1,7 @@
 ﻿using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DiscordBot.Commands
