e956c0ef26d25b50aa8c28201767385a05712caa
Kyle Givler <xvegan88x@gmail.com>
2021-05-22 10:04:20 -0400

    Why not?

---
+++ DiscordBot/Helpers/EmbedHelper.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Helpers
+{
+    public static class EmbedHelper
+    {
+        public static async Task<IMessage> SendEmbedAsync(this ISocketMessageChannel channel, string title, string description, string thumbImage = null)
+        {
+            var embed = new EmbedBuilder()
+                .WithTitle(title)
+                .WithDescription(description)
+                .WithColor(ColorHelper.GetColor())
+                .WithCurrentTimestamp();
+
+            if(thumbImage !=null)
+            {
+                embed.WithThumbnailUrl(thumbImage);
+            }
+
+            var message = await channel.SendMessageAsync(embed: embed.Build());
+            return message;
+        }
+    }
+}
