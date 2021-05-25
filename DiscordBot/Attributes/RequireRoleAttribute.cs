9dbef6ba6d36d15073fb873b12193c177aa239b3
Kyle Givler <xvegan88x@gmail.com>
2021-05-25 17:29:17 -0400

    Improved example (avoid cache issues)

--- DiscordBot/Attributes/RequireRoleAttribute.cs
+++ DiscordBot/Attributes/RequireRoleAttribute.cs
@@ -18,21 +18,22 @@ namespace DiscordBot.Attributes
         public RequireRoleAttribute(string name) => _name = name;
 
         // Override the CheckPermissions method
-        public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
+        public override async Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services)
         {
             // Check if this user is a Guild User, which is the only context where roles exist
             if (context.User is SocketGuildUser gUser)
             {
+                var user = await context.Guild.GetUserAsync(context.User.Id) as SocketGuildUser;
                 // If this command was executed by a user with the appropriate role, return a success
                 if (gUser.Roles.Any(r => r.Name == _name))
                     // Since no async work is done, the result has to be wrapped with `Task.FromResult` to avoid compiler errors
-                    return Task.FromResult(PreconditionResult.FromSuccess());
+                    return await Task.FromResult(PreconditionResult.FromSuccess());
                 // Since it wasn't, fail
                 else
-                    return Task.FromResult(PreconditionResult.FromError($"You must have a role named {_name} to run this command."));
+                    return await Task.FromResult(PreconditionResult.FromError($"You must have a role named {_name} to run this command."));
             }
             else
-                return Task.FromResult(PreconditionResult.FromError("You must be in a guild to run this command."));
+                return await Task.FromResult(PreconditionResult.FromError("You must be in a guild to run this command."));
         }
     }
 }
