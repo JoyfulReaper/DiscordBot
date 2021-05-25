daf390cbe041a8289bc2d1195fc223a02243467c
Kyle Givler <xvegan88x@gmail.com>
2021-05-25 17:24:45 -0400

    Require redditor role to do reddit stuffs

---
+++ DiscordBot/Helpers/RoleHelper.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+using System.Linq;
+
+namespace DiscordBot.Helpers
+{
+    public static class RoleHelper
+    {
+        public static bool CheckForRole(SocketGuildUser user, string role)
+        {
+            if (user.Roles.Any(r => r.Name.ToLowerInvariant() == role.ToLowerInvariant()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
