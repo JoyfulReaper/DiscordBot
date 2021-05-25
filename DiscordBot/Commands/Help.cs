d8db375d2c4fdc73fb244b8a23b9fc40779ed33e
Kyle Givler <xvegan88x@gmail.com>
2021-05-24 21:06:58 -0400

    Name attribute example

--- DiscordBot/Commands/Help.cs
+++ DiscordBot/Commands/Help.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace DiscordBot.Commands
 {
+    [Name("help!")]
     public class Help : InteractiveBase
     {
         private readonly CommandService _commandService;
