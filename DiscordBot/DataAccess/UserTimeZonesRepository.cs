991f50a388a76c010993397ba7dcce050b8b35e8
Kyle Givler <xvegan88x@gmail.com>
2021-05-31 20:16:43 -0400

    Add UserTimeZoneRepository

---
+++ DiscordBot/DataAccess/UserTimeZonesRepository.cs
@@ -0,0 +1,50 @@
+﻿using DiscordBot.DataAccess.SQLite;
+using DiscordBot.Models;
+using DiscordBot.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.DataAccess
+{
+    class UserTimeZonesRepository : Repository<UserTimeZone>
+    {
+        private readonly ISettings _settings;
+        private readonly ILogger<UserTimeZonesRepository> _logger;
+
+        public UserTimeZonesRepository(ISettings settings,
+            ILogger<UserTimeZonesRepository> logger) : base(settings, logger)
+        {
+            _settings = settings;
+            _logger = logger;
+        }
+
+        public async Task<UserTimeZone> GetByUserID(ulong userId)
+        {
+            var queryResult = await QueryFirstOrDefaultAsync<UserTimeZone>($"SELECT * FROM {TableName} " +
+                $"WHERE Id = @Id;", new { UserId = userId });
+
+            return queryResult;
+        }
+
+        public override async Task AddAsync(UserTimeZone entity)
+        {
+            await ExecuteAsync($"INSERT INTO {TableName} (UserId, TimeZone) " +
+                $"VALUES (@UserId, @TimeZone);", entity);
+        }
+
+        public override async Task DeleteAsync(UserTimeZone entity)
+        {
+            await ExecuteAsync($"DELETE FROM {TableName} WHERE ID = @Id;", entity);
+        }
+
+        public override async Task EditAsync(UserTimeZone entity)
+        {
+            await ExecuteAsync($"UPDATE {TableName} SET UserId = @UserId, TimeZone = @Timezone " +
+                $"WHERE Id = @Id;");
+        }
+    }
+}
