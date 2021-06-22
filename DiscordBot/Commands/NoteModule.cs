d6a2df10866a3628ca881b3a2c8b4cf2c1e41b89
Kyle Givler <xvegan88x@gmail.com>
2021-06-21 22:27:19 -0400

    Start of note module

---
+++ DiscordBot/Commands/NoteModule.cs
@@ -0,0 +1,75 @@
+﻿/*
+MIT License
+
+Copyright(c) 2021 Kyle Givler
+https://github.com/JoyfulReaper
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using Discord.Commands;
+using DiscordBotLib.DataAccess;
+using DiscordBotLib.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Commands
+{
+    [Name("Note")]
+    [Group("note")]
+    public class NoteModule : ModuleBase<SocketCommandContext>
+    {
+        private readonly INoteRepository _noteRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly ILogger<NoteModule> _logger;
+
+        public NoteModule(INoteRepository noteRepository,
+            IUserRepository userRepository,
+            ILogger<NoteModule> logger)
+        {
+            _noteRepository = noteRepository;
+            _userRepository = userRepository;
+            _logger = logger;
+        }
+
+        [Command("create")]
+        public async Task NoteCreate(string name, [Remainder] string text)
+        {
+            User user = await _userRepository.GetByUserId(Context.User.Id);
+            if(user == null)
+            {
+                user = new User { UserId = Context.User.Id, UserName = Context.User.Username };
+                await _userRepository.AddAsync(user);
+            }
+
+            if(user.UserName != Context.User.Username)
+            {
+                user.UserName = Context.User.Username;
+                await _userRepository.EditAsync(user);
+            }
+
+            Note note = new Note { Name = name, Text = text };
+            await _noteRepository.AddAsync(note, user);
+        }
+    }
+}
